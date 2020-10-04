dd37ecd8dc67fa714bf5d7e6b10d42826aee67a9
United Geekdom <53636961+united-geekdom@users.noreply.github.com>
2020-10-04 09:19:46 +0530

    Update yt.js

--- yt.js
+++ yt.js
@@ -4,4 +4,8 @@ code = code.split("=");
 code = code[1];
 code = code.split("&");
 code = code[0];
+if(document.getElementsByClassName('yt-playability-error-supported-renderers').length != 0) {
 document.getElementsByClassName('yt-playability-error-supported-renderers')[0].innerHTML = "<iframe src='https://www.youtube.com/embed/" + code + "' allowfullscreen width='100%' height='100%'>";
+} else {
+ alert("Please use this on an age-restricted video"); 
+}
