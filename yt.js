7063de186680ef266f4c8a481b3d9722f36298b1
United Geekdom <53636961+united-geekdom@users.noreply.github.com>
2020-10-05 19:00:56 +0530

    Update yt.js

--- yt.js
+++ yt.js
@@ -5,7 +5,7 @@ code = code.split("=");
 code = code[1];
 code = code.split("&");
 code = code[0];
-document.getElementsByClassName('yt-playability-error-supported-renderers')[0].innerHTML = "<iframe src='https://www.youtube.com/embed/" + code + "?autoplay=1&mute=0&controls=1&start=3&origin=https%3A%2F%2Fdebatecentral.wixsite.com&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1&widgetid=1' allowfullscreen width='100%' height='100%'>";
+document.getElementsByClassName('yt-playability-error-supported-renderers')[0].innerHTML = "<iframe src='https://www.youtube.com/embed/" + code + "?autoplay=1&mute=0&controls=1&start=0&origin=https%3A%2F%2Fdebatecentral.wixsite.com&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1&widgetid=1' allowfullscreen width='100%' height='100%'>";
 } else {
  alert("Please use this on an age-restricted video"); 
 }
